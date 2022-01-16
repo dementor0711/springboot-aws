2f6e67a714e70fda0bc90e91b2da989ba6a2f6f5
dementor <dementor@gmail.com>
2022-01-16 13:31:11 +0900

    Initial commit

---
+++ src/main/java/com/dementor/springbootaws/SpringbootwithawsApplication.java
@@ -0,0 +1,13 @@
+package com.dementor.springbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwithawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dementor/springbootaws/SpringbootwithawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dementor.springbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwithawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
